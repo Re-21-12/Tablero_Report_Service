c60419995c875fb799598f84b341534e265a9c99
re-21-12 <victora.caas@gmail.com>
2025-10-14 20:51:13 -0600

    [UPDATED] update chaingw

--- Report_APi/encabezado.py
+++ Report_APi/encabezado.py
@@ -2,19 +2,22 @@ from reportlab.lib import colors
 from reportlab.lib.units import inch
 from reportlab.platypus import Image, Paragraph, Spacer, Table, TableStyle
 from datetime import datetime
+import os
 
 def encabezado_pdf(contenido, estilos, titulo_encabezado):
     '''Agrega encabezado estándar con logo, título y fecha.'''
-    try:
-        logo_path = 'D:\\Universidad\\Logo.png'  
-        img = Image(logo_path, width=1*inch, height=1*inch)
-    except Exception:
-        img = Paragraph('🏀', estilos['Title']) 
+    logo_path = 'D:\\Universidad\\Logo.png'
+    if os.path.exists(logo_path):
+        try:
+            img = Image(logo_path, width=1*inch, height=1*inch)
+        except Exception:
+            img = Spacer(1, 1*inch)  # Espacio vacío si hay error al cargar la imagen
+    else:
+        img = Spacer(1, 1*inch)  # Espacio vacío si no existe el archivo
 
     titulo = Paragraph(f"<b>{titulo_encabezado}</b>", estilos['Title'])
     fecha = Paragraph(f"<font size=10>Generado el {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}</font>", estilos['Normal'])
 
-    
     header_table = Table([[img, titulo, fecha]], colWidths=[80, 300, 160])
     header_table.setStyle(TableStyle([
         ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
@@ -24,7 +27,6 @@ def encabezado_pdf(contenido, estilos, titulo_encabezado):
     contenido.append(header_table)
     contenido.append(Spacer(1, 0.2 * inch))
 
-    
     contenido.append(Table(
         [['']], colWidths=[540],
         style=[('LINEBELOW', (0, 0), (-1, -1), 1, colors.HexColor('#1F4E78'))]
